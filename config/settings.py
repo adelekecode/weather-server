cdf8b72a630cf3cd4c0f7605d93eef279b5c1a05
adelekecode <femiadeleke601@gmail.com>
2024-11-18 11:33:52 +0100

    allowed host update

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['weather-server-production-48e8.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://weather-server-production-48e8.up.railway.app']
 
 
 # Application definition
